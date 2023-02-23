7a6e01bd49b6db740a5c45da45fcd413dd35873b
Kesley Santana Santos <kesleysantana2469@gmail.com>
2023-02-23 20:29:29 -0300

    commit

--- src/pages/Home/components/Card/index.tsx
+++ src/pages/Home/components/Card/index.tsx
@@ -18,79 +18,66 @@ export interface ICoffee {
     value: number,
     quantity: number,
     selected: boolean,
-    onAddCoffeeSelected(id:string): void
 }
 
-// interface CardCoffee {
-//     coffee:ICoffee,
-//     selected: boolean,
-//     value: number,
-//     quantity: number,
-//     onRemoveCoffee(id:string):void,
-//     onAddCoffeeSelected: (id:string) => void
-// }
+interface CardCoffee {
+    dataCoffee:ICoffee,
+    onAddCoffeeSelected(dataCoffee:{}):void,
+    onChangeQuantity(quantity:number, value:number):void
+}
 
 
 
-export function CoffeeCard({ ...props }:ICoffee) {
-    const [quantityCoffee, setQuantityCoffee] = useState(1)
-
+export function CoffeeCard({dataCoffee, onAddCoffeeSelected, onChangeQuantity}:CardCoffee) {
+const [quantityCoffee, setQuantityCoffee] = useState<number>(1)
+const [valueCoffee, setValueCoffee] = useState<number>(9.65)
 
     
-    const coffeeValueCalc = (9.65 * quantityCoffee).toFixed(2)
-    const coffeeValue = parseFloat(coffeeValueCalc)
-    
-    
-    function handleAddQuantity() {
-        if( quantityCoffee < 10) {
-            setQuantityCoffee(quantityCoffee + 1)
-        }
+
+function handleAddCoffeeSelected() {
+    onAddCoffeeSelected(dataCoffee)
+}
+
+function handleChangeQuantity(mode: 'sub' | 'add') {
+
+    if(mode === 'sub' && quantityCoffee > 1) {
+
+        const quantity = quantityCoffee - 1
+        const teste = (valueCoffee - 9.65).toFixed(2)
+        const coffeeValue = parseFloat(teste)
+        setValueCoffee(coffeeValue)
+        setQuantityCoffee(quantity)
+        onChangeQuantity(quantity, coffeeValue) 
+
     }
-    
-    function handleReduceQuantity() {
-        if (quantityCoffee > 1) {
-            setQuantityCoffee(quantityCoffee - 1)
-        }
+    if(mode === 'add' && quantityCoffee < 10) {
+
+        const quantity = quantityCoffee + 1
+        const teste = (valueCoffee + 9.65).toFixed(2)
+        const coffeeValue = parseFloat(teste)
+        setValueCoffee(coffeeValue)
+        setQuantityCoffee(quantity)
+        onChangeQuantity(quantity, coffeeValue)
+
     }
-    
-    
-    function handleAddCoffeeSelected() {
-        props.onAddCoffeeSelected(props.id)
-    }
-
-
-    
-    // function handleAddCoffeeSelected() {
-
-    //     const cardSelected = {
-    //         name: props.name,
-    //         id: props.id,
-    //         type: props.type,
-    //         src: props.src,
-    //         info: props.info,
-    //         value: props.value,
-    //         quantity: props.quantity,
-    //         selected: true     
-    //     }
-    // }
-
+}
 
     
 
 
     return(
         <Card>
-         <img src={props.src} alt="" />
-         <span>{props.type}</span>
-         <h2>{props.name}</h2> 
-         <p>{props.info}</p>
+         <img src={dataCoffee.src} alt="" />
+         <span>{dataCoffee.type}</span>
+         <h2>{dataCoffee.name}</h2> 
+         <p>{dataCoffee.info}</p>
 
          <BuyContainer>
-            <Price>R$<PriceAmount>{coffeeValue}</PriceAmount></Price>
+            <Price>R$<PriceAmount>{valueCoffee}</PriceAmount></Price>
             <Counter>
-                <Minus onClick={handleReduceQuantity} size={14} weight="bold" color="#8047F8" cursor="pointer"/>
+                <Minus onClick={() => handleChangeQuantity('sub')} size={14} weight="bold" color="#8047F8" cursor="pointer"/>
                 <Amount>{quantityCoffee}</Amount>
-                <Plus onClick={handleAddQuantity} size={14} weight="bold" color="#8047F8" cursor="pointer" />
+                <Plus onClick={() => handleChangeQuantity('add')} size={14} weight="bold" color="#8047F8" cursor="pointer" />
             </Counter>
             <button onClick={() => handleAddCoffeeSelected()} >
                 <ShoppingCartSimple size={22} weight="fill" color="#F3F2F2" />
--- src/pages/Home/components/Shop/index.tsx
+++ src/pages/Home/components/Shop/index.tsx
@@ -22,85 +22,66 @@ import { useState } from "react";
 
 
 const cardProps:ICoffee[] = [
-    {id:uuidv4(),  src:ExpressoTradicional, type:"Tradicional", name:"Expresso Tradicional", info:"O tradicional café feito com água quente e grãos moídos", quantity: 0, value: 0, selected: false, onAddCoffeeSelected(id:string):void {
-
-    }},
+    { id:uuidv4(),  src:ExpressoTradicional, type:"Tradicional", name:"Expresso Tradicional", info:"O tradicional café feito com água quente e grãos moídos", quantity: 1, value: 9.65, selected: false },
     
-    {id:uuidv4(), src:ExpressoAmericano, type:"Tradicional", name:"Expresso Americano", info:"Expresso diluído, menos intenso que o tradicional", quantity: 0, value: 0, selected: false, onAddCoffeeSelected(id:string):void {
-        
-    }, },
+    { id:uuidv4(), src:ExpressoAmericano, type:"Tradicional", name:"Expresso Americano", info:"Expresso diluído, menos intenso que o tradicional", quantity: 1, value: 9.65, selected: false },
 
-    {id:uuidv4(), src:ExpressoCremoso, type:"Tradicional", name:"Expresso Cremoso", info:"Café expresso tradicional com espuma cremosa", quantity: 0, value: 0, selected: false, onAddCoffeeSelected(id:string):void {
-        
-    }, },
+    { id:uuidv4(), src:ExpressoCremoso, type:"Tradicional", name:"Expresso Cremoso", info:"Café expresso tradicional com espuma cremosa", quantity: 1, value: 9.65, selected: false },
 
-    {id:uuidv4(), src:ExpressoGelado, type:"Tradicional", name:"Expresso Gelado", info:"Bebida preparada com café expresso e cubos de gelo", quantity: 0, value: 0, selected: false, onAddCoffeeSelected(id:string){
+    { id:uuidv4(), src:ExpressoGelado, type:"Tradicional", name:"Expresso Gelado", info:"Bebida preparada com café expresso e cubos de gelo", quantity: 1, value: 9.65, selected: false },
 
-    }, },
+    { id:uuidv4(), src:CafeComLeite, type:"Tradicional", name:"Café com Leite", info:"Meio a meio de expresso tradicional com leite vaporizado", quantity: 1, value: 9.65, selected: false },
 
-    {id:uuidv4(), src:CafeComLeite, type:"Tradicional", name:"Café com Leite", info:"Meio a meio de expresso tradicional com leite vaporizado", quantity: 0, value: 0, selected: false, onAddCoffeeSelected(id:string):void {
-        
-    }, },
+    { id:uuidv4(), src:Latte, type:"Tradicional", name:"Latte", info:"Uma dose de café expresso com o dobro de leite e espuma cremosa", quantity: 1, value: 9.65, selected: false },
 
-    {id:uuidv4(), src:Latte, type:"Tradicional", name:"Latte", info:"Uma dose de café expresso com o dobro de leite e espuma cremosa", quantity: 0, value: 0, selected: false, onAddCoffeeSelected(id:string):void {
-        
-    }, },
+    { id:uuidv4(), src:Capuccino, type:"Tradicional", name:"Capuccino", info:"Bebida com canela feita de doses iguais de café, leite e espuma", quantity: 1, value: 9.65, selected: false },
 
-    {id:uuidv4(), src:Capuccino, type:"Tradicional", name:"Capuccino", info:"Bebida com canela feita de doses iguais de café, leite e espuma", quantity: 0, value: 0, selected: false, onAddCoffeeSelected(id:string):void {
-        
-    }, },
+    { id:uuidv4(), src:Macchiato, type:"Tradicional", name:"Macchiato", info:"Café expresso misturado com um pouco de leite quente e espuma", quantity: 1, value: 9.65, selected: false },
 
-    {id:uuidv4(), src:Macchiato, type:"Tradicional", name:"Macchiato", info:"Café expresso misturado com um pouco de leite quente e espuma", quantity: 0, value: 0, selected: false, onAddCoffeeSelected(id:string):void {
-        
-    }, },
+    { id:uuidv4(), src:Mocaccino, type:"Tradicional", name:"Mocaccino", info:"Café expresso com calda de chocolate, pouco leite e espuma", quantity: 1, value: 9.65, selected: false },
 
-    {id:uuidv4(), src:Mocaccino, type:"Tradicional", name:"Mocaccino", info:"Café expresso com calda de chocolate, pouco leite e espuma", quantity: 0, value: 0, selected: false, onAddCoffeeSelected(id:string):void {
-        
-    }, },
+    { id:uuidv4(), src:ChocolateQuente, type:"Tradicional", name:"Chocolate Quente", info:"Bebida feita com chocolate dissolvido no leite quente e café", quantity: 1, value: 9.65, selected: false },
 
-    {id:uuidv4(), src:ChocolateQuente, type:"Tradicional", name:"Chocolate Quente", info:"Bebida feita com chocolate dissolvido no leite quente e café", quantity: 0, value: 0, selected: false, onAddCoffeeSelected(id:string):void {
-        
-    }, },
+    { id:uuidv4(), src:Cubano, type:"Tradicional", name:"Cubano", info:"Drink gelado de café expresso com rum, creme de leite e hortelã", quantity: 1, value: 9.65, selected: false },
 
-    {id:uuidv4(), src:Cubano, type:"Tradicional", name:"Cubano", info:"Drink gelado de café expresso com rum, creme de leite e hortelã", quantity: 0, value: 0, selected: false, onAddCoffeeSelected(id:string):void {
-        
-    }, },
+    { id:uuidv4(), src:Havaiano, type:"Tradicional", name:"Havaiano", info:"Bebida adocicada preparada com café e leite de coco", quantity: 1, value: 9.65, selected: false },
 
-    {id:uuidv4(), src:Havaiano, type:"Tradicional", name:"Havaiano", info:"Bebida adocicada preparada com café e leite de coco", quantity: 0, value: 0, selected: false, onAddCoffeeSelected(id:string):void {
-        
-    }, },
+    { id:uuidv4(), src:Arabe, type:"Tradicional", name:"Árabe", info:"Bebida preparada com grãos de café árabe e especiarias", quantity: 1, value: 9.65, selected: false },
 
-    {id:uuidv4(), src:Arabe, type:"Tradicional", name:"Árabe", info:"Bebida preparada com grãos de café árabe e especiarias", quantity: 0, value: 0, selected: false, onAddCoffeeSelected(id:string):void {
-        
-    }, },
-
-    {id:uuidv4(), src:Irlandes, type:"Tradicional", name:"Irlandês", info:"Bebida a base de café, uísque irlandês, açúcar e chantilly", quantity: 0, value: 0, selected: false, onAddCoffeeSelected(id:string):void {
-        
-    }, }
+    { id:uuidv4(), src:Irlandes, type:"Tradicional", name:"Irlandês", info:"Bebida a base de café, uísque irlandês, açúcar e chantilly", quantity: 1, value: 9.65, selected: false }
 ]
 
 
-export function Shop({ ...props }:ICoffee) {
-// const [product, setProducts] = useState(cardProps as ICoffee[])
+export function Shop(props:ICoffee) {
 const [MyCart, setMyCart] = useState([] as ICoffee[])
 
 
 
 
+                            //LISTA NÃO ATUALIZANDO
 
-function handleAddCoffeeSelected() {
+function handleAddCoffeeSelected(coffee:ICoffee) {
 
-    const cardSelected = {
-        name: 'kesley',
-        // id: props.id,
-        // type: props.type,
-        // src: props.src,
-        // info: props.info,
-        // value: props.value,
-        // quantity: props.quantity,
-        selected: true     
+    const coffeeSelected = {
+        id: coffee.id,
+        value: coffee.value,
+        name: coffee.name,
+        selected: coffee.selected = true,
+        quantity: coffee.quantity,
+        type: coffee.type,
+        src: coffee.src,
+        info: coffee.info
     }
-    console.log(cardSelected)
+    setMyCart([...MyCart, coffeeSelected])
+    console.log(MyCart)
+}
+
+function onChangeQuantity(id:string, quantity:number, value:number) {
+    const coffeeFound = cardProps.find(item => item.id === id)
+    if(coffeeFound){
+        coffeeFound.quantity = quantity
+        coffeeFound.value = value
+    }
 }
 
 
@@ -111,15 +92,9 @@ function handleAddCoffeeSelected() {
                 {cardProps.map((product) => (
                     <CoffeeCard 
                      key={product.id}
-                     id={product.id}
-                     src={product.src}
-                     type={product.type}
-                     name={product.name}
-                     info={product.info}
-                     quantity={product.quantity}
-                     selected={product.selected}
-                     value={product.value}
-                     onAddCoffeeSelected={handleAddCoffeeSelected}
+                     dataCoffee={{...product}}
+                     onChangeQuantity={(quantity:number, value:number)=>onChangeQuantity(product.id, quantity, value)}
+                     onAddCoffeeSelected={()=>handleAddCoffeeSelected(product)}
                     />
                 ))}
             </CardsContainer>
